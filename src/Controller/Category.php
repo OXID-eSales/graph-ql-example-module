613a7faf725f74af7e1e35d1c6c9ffe7a4d4296e
michael.koltan@oxid-esales.com <michael.koltan@oxid-esales.com>
2019-11-18 14:40:22 +0100

    Remove stale code

--- src/Controller/Category.php
+++ src/Controller/Category.php
@@ -12,7 +12,6 @@ namespace OxidEsales\GraphQL\Example\Controller;
 use OxidEsales\GraphQL\Base\Service\LegacyServiceInterface;
 use OxidEsales\GraphQL\Example\Dao\CategoryDaoInterface;
 use OxidEsales\GraphQL\Example\DataObject\Category as CategoryDataObject;
-use OxidEsales\GraphQL\Example\DataObject\CategoryInput;
 use TheCodingMachine\GraphQLite\Annotations\Logged;
 use TheCodingMachine\GraphQLite\Annotations\Mutation;
 use TheCodingMachine\GraphQLite\Annotations\Query;
@@ -67,7 +66,7 @@ class Category
     }
 
     /**
-     * create a category
+     * create or update a category
      *
      * @Mutation()
      * @Logged()
@@ -81,22 +80,4 @@ class Category
             $this->legacyService->getShopId()
         );
     }
-
-    /**
-     * create a category
-     *
-     * @Mutation()
-     * @Logged()
-     * @Right("CATEGORY_CREATE")
-     */
-    public function alterName(string $id, string $name): CategoryDataObject
-    {
-        return $this->categoryDao->alterName(
-            $id,
-            $name,
-            $this->legacyService->getLanguageId(),
-            $this->legacyService->getShopId()
-        );
-    }
-
 }
